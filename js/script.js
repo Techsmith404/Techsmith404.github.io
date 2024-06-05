ba1812a7db151521804b030812d20175f6743cbb
techsmith404 <techsmith404@gmail.com>
2024-06-05 00:56:41 -0500

    modified:   about.html 	modified:   contact.html 	modified:   css/styles.css 	modified:   index.html 	modified:   js/script.js 	modified:   projects.html

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ var menuOpen = false;
 function expand() {
   var menu = document.getElementById("menu");
   var cover = document.getElementById('menuCover');
+  var nav = document.getElementById('navMenu');
 
   if (menuOpen != true) { 
     menu.classList.remove("menuC");
     menu.classList.add("menuO");
     cover.classList.toggle('hidden');
+    setTimeout(() => { nav.classList.toggle('hidden');}, 500);
     menuOpen = true;
 } else {
     menu.classList.remove("menuO");
     menu.classList.add("menuC");
+    setTimeout(() => { nav.classList.toggle('hidden');}, 500);
     cover.classList.toggle('hidden');
     menuOpen = false;
   }
