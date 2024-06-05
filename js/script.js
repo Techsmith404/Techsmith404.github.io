07be22a386cd3c1f23826886289915b2d0d1bfc4
techsmith404 <techsmith404@gmail.com>
2024-06-04 19:06:38 -0500

    update

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ var menuOpen = false;
 
 function expand() {
   var menu = document.getElementById("menu");
-  var banner = document.getElementsByClassName('menuBanner');
+  var cover = document.getElementById('menuCover');
 
   if (menuOpen != true) { 
     menu.classList.remove("menuC");
     menu.classList.add("menuO");
+    cover.classList.toggle('hidden');
     menuOpen = true;
 } else {
     menu.classList.remove("menuO");
     menu.classList.add("menuC");
+    cover.classList.toggle('hidden');
     menuOpen = false;
   }
 }
